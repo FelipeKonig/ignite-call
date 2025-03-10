4e418d0726a2958683f017bd3849e6513c75ee95
felipe konig <lipekonig@gmail.com>
2025-03-10 00:10:10 -0300

    styling of the calendar page created

---
+++ src/components/Calendar/index.tsx
@@ -0,0 +1,90 @@
+import { CaretLeft, CaretRight } from 'phosphor-react'
+import {
+  CalendarActions,
+  CalendarBody,
+  CalendarContainer,
+  CalendarDay,
+  CalendarHeader,
+  CalendarTitle,
+} from './styles'
+import { getWeekDays } from '@/src/utils/get-week-days'
+
+export function Calendar() {
+  const shortWeekDays = getWeekDays({ short: true })
+
+  return (
+    <CalendarContainer>
+      <CalendarHeader>
+        <CalendarTitle>
+          Março <span>2025</span>
+        </CalendarTitle>
+        <CalendarActions>
+          <button>
+            <CaretLeft />
+          </button>
+          <button>
+            <CaretRight />
+          </button>
+        </CalendarActions>
+      </CalendarHeader>
+
+      <CalendarBody>
+        <thead>
+          <tr>
+            {shortWeekDays.map((weekDay) => (
+              <th key={weekDay}>{weekDay}.</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>2</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>3</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>4</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay disabled>5</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>6</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>7</CalendarDay>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <CalendarDay>8</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>9</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay disabled>10</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>11</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>12</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>13</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>14</CalendarDay>
+            </td>
+          </tr>
+        </tbody>
+      </CalendarBody>
+    </CalendarContainer>
+  )
+}
---
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.page.tsx
@@ -0,0 +1,39 @@
+import { Calendar } from '@/src/components/Calendar'
+import {
+  Container,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList,
+} from './styles'
+
+export function CalendarStep() {
+  const isDateSelected = true
+
+  return (
+    <Container isTimePickerOpen={isDateSelected}>
+      <Calendar />
+
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            segunda-feira <span>09 de março</span>
+          </TimePickerHeader>
+          <TimePickerList>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>09:00</TimePickerItem>
+            <TimePickerItem>10:00</TimePickerItem>
+            <TimePickerItem>11:00</TimePickerItem>
+            <TimePickerItem>12:00</TimePickerItem>
+            <TimePickerItem>13:00</TimePickerItem>
+            <TimePickerItem>14:00</TimePickerItem>
+            <TimePickerItem>15:00</TimePickerItem>
+            <TimePickerItem>16:00</TimePickerItem>
+            <TimePickerItem>17:00</TimePickerItem>
+            <TimePickerItem>18:00</TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
+    </Container>
+  )
+}
